a4573bb37f3cc27486cc3ec2adbe3d5dc984fb06
joão pedro cruz <joaocruzzdev@gmail.com>
2025-01-08 11:24:28 -0300

    componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { Header } from './components/header'
+
 import './global.css'
+import styles from "./app.module.css"
+import { Sidebar } from './components/sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main> post </main>
+      </div>
     </div>
   )
 }
