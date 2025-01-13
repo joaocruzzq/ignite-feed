8c8eec74935a5827f59bf05659224355e41bd546
joão pedro cruz <joaocruzzdev@gmail.com>
2025-01-13 16:26:49 -0300

    validando formulario de comentario

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -33,7 +33,12 @@ export function Post({ author, publishedAt, content }) {
    }
 
    function handleNewCommentChange() {
+      event.target.setCustomValidity('')
       setNewCommentText(event.target.value)
+   }
+
+   function handleNewCommentInvalid() {
+      event.target.setCustomValidity('Esse campo é obrigatório')
    }
 
    function deleteComment(commentToDelete) {
@@ -43,6 +48,8 @@ export function Post({ author, publishedAt, content }) {
 
       setComments(commentsWithoutDeletedOne)
    }
+
+   const isNewCommentEmpty = newCommentText.length === 0
 
    return (
       <article className={styles.post}>
@@ -81,10 +88,12 @@ export function Post({ author, publishedAt, content }) {
                value={newCommentText}
                placeholder="Deixe um comentário"
                onChange={handleNewCommentChange}
+               onInvalid={handleNewCommentInvalid}
+               required
             />
 
             <footer>
-               <button type="submit">Publicar</button>
+               <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
             </footer>
          </form>
          
